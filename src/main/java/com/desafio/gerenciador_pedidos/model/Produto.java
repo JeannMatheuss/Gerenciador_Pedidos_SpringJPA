42d4f54fcf494ea390414152cf2e2e5d47d020ff
Jean Matheus <jeanmatheus0612@gmail.com>
2025-07-17 00:47:01 -0300

    Usando paramâtros da anotação Columnn

--- src/main/java/com/desafio/gerenciador_pedidos/model/Produto.java
+++ src/main/java/com/desafio/gerenciador_pedidos/model/Produto.java
@@ -1,16 +1,15 @@
 package com.desafio.gerenciador_pedidos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String nome;
+    @Column(name = "valor")
     private double preco;
 
     public Produto(Long id, String nome, double preco) {
