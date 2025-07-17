2c57e94474ed4e7f27efe2e6d799b351f055d920
Jean Matheus <jeanmatheus0612@gmail.com>
2025-07-17 00:41:24 -0300

    Usando o genratedvalue com a estratégia de geração automática de identificadores

--- src/main/java/com/desafio/gerenciador_pedidos/model/Produto.java
+++ src/main/java/com/desafio/gerenciador_pedidos/model/Produto.java
@@ -1,11 +1,14 @@
 package com.desafio.gerenciador_pedidos.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Produto {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private double preco;
